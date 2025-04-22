9cfb1877537c3efbba77d56eb9cf7434605716ee
Masrafi404 <mdmasrafi902@gmail.com>
2025-04-23 05:43:14 +0600

    same functionality with strcmp

--- C_Compare.c
+++ C_Compare.c
@@ -4,52 +4,37 @@ int main()
     char a[21], b[21];
     scanf("%s%s", a, b);
 
-    // for (int i = 0; 1;)
-    // {
-    //     if (a[i] == '\0')
-    //     {
-    //         printf("%s", a);
-    //         break;
-    //     }
-    //     else if (b[i] == '\0')
-    //     {
-    //         printf("%s", b);
-    //         break;
-    //     }
-    //     else if (a[i] < b[i])
-    //     {
-    //         printf("%s", a);
-    //         break;
-    //     }
-    //     else if (a[i] > b[i])
-    //     {
-    //         printf("%s", b);
-    //         break;
-    //     }
-    //     else if (a[i] == '\0' && a[i] == '\0')
-    //     {
-    //         printf("%s", a);
-    //         break;
-    //     }
-    //     else if (a[i] == b[i])
-    //     {
-    //         i++;
-    //     }
-    // }
-
-    int val = strcmp(a, b);
-
-    if (val < 0)
+    for (int i = 0; 1;)
     {
-        printf("%s", a);
-    }
-    else if (val == 0)
-    {
-        printf("%s", a);
-    }
-    else if (val > 0)
-    {
-        printf("%s", b);
+        if (a[i] == '\0')
+        {
+            printf("%s", a);
+            break;
+        }
+        else if (b[i] == '\0')
+        {
+            printf("%s", b);
+            break;
+        }
+        else if (a[i] < b[i])
+        {
+            printf("%s", a);
+            break;
+        }
+        else if (a[i] > b[i])
+        {
+            printf("%s", b);
+            break;
+        }
+        else if (a[i] == '\0' && a[i] == '\0')
+        {
+            printf("%s", a);
+            break;
+        }
+        else if (a[i] == b[i])
+        {
+            i++;
+        }
     }
 
     return 0;
